898fbbfdad853b7c7f908aa4196745f0f8a824bd
Nikhil Kumar Singh <85548554+NikhilSingh71@users.noreply.github.com>
2024-01-05 18:51:49 +0530

    Update TopNavbar.jsx

--- src/components/TopNavbar.jsx
+++ src/components/TopNavbar.jsx
@@ -1,43 +1,30 @@
+// TopNavbar.js
 
-import { makeStyles } from '@material-ui/core/styles';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import React, { useEffect, useState } from "react";
 
-const useStyles = makeStyles((theme) => ({
-  tNavbar: {
-    display: 'flex',
-    justifyContent: 'center',
-    marginBottom: theme.spacing(2), // Adjust the spacing as needed
-  },
-  tab: {
-    marginRight: theme.spacing(2), // Adjust the spacing between tabs as needed
-  },
-}));
-
-const TopNavbar = ({getTabNumber}) => {
-  const classes = useStyles();
+const TopNavbar = ({ getTabNumber }) => {
   const [activeTab, setActiveTab] = useState(0);
-
 
   const handleTabChange = (event, newValue) => {
     setActiveTab(newValue);
   };
 
   useEffect(() => {
-    getTabNumber(activeTab)
+    getTabNumber(activeTab);
   }, [activeTab]);
 
   return (
-    <div className={classes.tNavbar}>
+    <div className="tNavbar"> 
       <Tabs
         value={activeTab}
         onChange={handleTabChange}
         indicatorColor="primary"
         textColor="primary"
       >
-        <Tab label="Inbox" className={classes.tab} />
-        <Tab label="All Calls" className={classes.tab} />
+        <Tab label="Inbox" className="tab" /> 
+        <Tab label="All Calls" className="tab" /> 
       </Tabs>
     </div>
   );
